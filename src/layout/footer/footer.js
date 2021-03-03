57a675a7b47d5a8cbb7f8b314532c310f25bf693
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-03-03 15:47:07 +0000

    Footer and Navbar

--- src/layout/footer/footer.js
+++ src/layout/footer/footer.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./footer.module.css";
 
-const footer = (props) => <div className={styles.Footer}></div>;
+const footer = (props) => (
+    <div className={styles.Footer}>
+        <span> &copy;</span> BioVisualSpeech
+    </div>
+);
 
 export default footer;
