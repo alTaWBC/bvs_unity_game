e3ad5cd18c3023c2209a7bcd098ba68796ed8830
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-03-03 23:41:12 +0000

    Form UI is ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import styles from "./App.module.css";
 import Navbar from "./layout/navbar/navbar";
 import Footer from "./layout/footer/footer";
-// import Login from "./pages/login/Login";
-import Form from "./pages/form/Form";
+import Login from "./pages/login/Login";
+// import Form from "./pages/form/Form";
 
 function App() {
     return (
         <div className={styles.App}>
             <Navbar />
-            {/* <Login /> */}
-            <Form />
+            <Login />
+            {/* <Form /> */}
             <Footer />
         </div>
     );
--- src/pages/form/Form.js
+++ src/pages/form/Form.js
@@ -34,25 +34,27 @@ class Form extends Component {
     render() {
         return (
             <div className={styles.Form}>
-                <h1>Formulário de Criança</h1>
-                <TextInput name="Id da Criança" id="IdCrianca" />
-                <TextInput name="Idade da Criança" id="IdadeCrianca" />
-                <Checkbox
-                    name="Português é Língua Mãe?"
-                    id="estrangeiro"
-                    checked={this.state.estrangeiro}
-                    onClick={this.onClickEstrangeiro}
-                />
-                <Checkbox
-                    name="Desvios na fala?"
-                    id="desvios"
-                    checked={this.state.desvios}
-                    onClick={this.onClickDesvios}
-                />
-                <Radio options={radioOptions} name="Gender" text="Género" />
-                {this.state.estrangeiro ? <TextInput name="Nacionalidade" id="Nacionalidade" /> : null}
-                {this.state.desvios ? <TextInput name="Desvios Na Fala" id="Desvios" /> : null}
-                <button>Enviar</button>
+                <div className={styles.Box}>
+                    <h1>Formulário de Criança</h1>
+                    <TextInput name="Id da Criança" id="IdCrianca" />
+                    <TextInput name="Idade da Criança" id="IdadeCrianca" />
+                    <Checkbox
+                        name="Português é Língua Mãe?"
+                        id="estrangeiro"
+                        checked={this.state.estrangeiro}
+                        onClick={this.onClickEstrangeiro}
+                    />
+                    <Checkbox
+                        name="Desvios na fala?"
+                        id="desvios"
+                        checked={this.state.desvios}
+                        onClick={this.onClickDesvios}
+                    />
+                    <Radio options={radioOptions} name="Gender" text="Género" />
+                    {this.state.estrangeiro ? <TextInput name="Nacionalidade" id="Nacionalidade" /> : null}
+                    {this.state.desvios ? <TextInput name="Desvios Na Fala" id="Desvios" /> : null}
+                    <button>Enviar</button>
+                </div>
             </div>
         );
     }
