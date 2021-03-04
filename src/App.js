ff53211ff81aae5678d5e9fd4f74d8dc6c66ffc3
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-03-04 14:20:56 +0000

    Basic Login Working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import styles from "./App.module.css";
 import Navbar from "./layout/navbar/navbar";
 import Footer from "./layout/footer/footer";
-// import Login from "./pages/login/Login";
+import Login from "./pages/login/Login";
 import Form from "./pages/form/Form";
+import { Component } from "react";
 
-function App() {
-    return (
-        <div className={styles.App}>
-            <Navbar />
-            {/* <Login /> */}
-            <Form />
-            <Footer />
-        </div>
-    );
+const PASSWORD_HASH = "biovisualspeech";
+
+class App extends Component {
+    state = {
+        page: "Login",
+    };
+
+    getPage = (page) => {
+        switch (page) {
+            case "Login":
+                return <Login login={this.login} />;
+            case "Form":
+                return <Form />;
+            case "Game":
+            default:
+                return <div></div>;
+        }
+    };
+
+    hashPassword = (password) => {
+        return password;
+    };
+
+    login = (password) => {
+        const passwordHash = this.hashPassword(password);
+        if (passwordHash !== PASSWORD_HASH) return;
+        this.setState({ page: "Form" });
+    };
+
+    changePage = (page) => {
+        this.setState({ page });
+    };
+
+    render() {
+        const page = this.getPage(this.state.page);
+        return (
+            <div className={styles.App}>
+                <Navbar />
+                {page}
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 import styles from "./Login.module.css";
 
 class Login extends Component {
-    state = {};
+    state = {
+        password: "",
+    };
+
+    onChangePassword = ({ target: { value: password } }) => {
+        this.setState({ password });
+    };
 
     render() {
         return (
@@ -10,13 +16,11 @@ class Login extends Component {
                 <div className={styles.Content}>
                     <h2>Login</h2>
                     <div className={styles.Form}>
-                        <form action="#">
-                            <div className={styles.input}>
-                                <label htmlFor="password">Password</label>
-                                <input type="password" id="password" />
-                            </div>
-                            <button>Submit</button>
-                        </form>
+                        <div className={styles.input}>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" onChange={this.onChangePassword} />
+                        </div>
+                        <button onClick={() => this.props.login(this.state.password)}>Submit</button>
                     </div>
                 </div>
             </div>
