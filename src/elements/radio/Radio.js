ad4536d9f3b60067a5b7337738a7169e98069bbe
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-03-04 15:51:22 +0000

    Changed Radio to have default value

--- src/elements/radio/Radio.js
+++ src/elements/radio/Radio.js
@@ -4,10 +4,17 @@ import styles from "./Radio.module.css";
 const Radio = (props) => (
     <div className={styles.Radio}>
         <p>{props.text}</p>
-        {props.options.map((object) => (
+        {props.options.map((object, index) => (
             <div className={styles.option} key={object.value}>
                 <label htmlFor={object.name}>{object.name}</label>
-                <input onChange={props.onChange} type="radio" name={props.name} id={object.name} value={object.value} />
+                <input
+                    onChange={props.onChange}
+                    type="radio"
+                    name={props.name}
+                    id={object.name}
+                    value={object.value}
+                    defaultChecked={index === 0}
+                />
             </div>
         ))}
     </div>
