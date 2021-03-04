adb2a10f8a4b793b5ea6247edb1ddf1bb65f459d
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-03-04 15:51:51 +0000

    Now Icon and titles are correct

--- src/layout/navbar/navbar.js
+++ src/layout/navbar/navbar.js
@@ -3,7 +3,7 @@ import styles from "./navbar.module.css";
 
 const navbar = (props) => (
     <div className={styles.Navbar}>
-        <h3 className={styles.title}>BioVisualSpeech</h3>
+        <h3 className={styles.title}>VisualSpeech Sibilants Game</h3>
         <div className={styles.pages}>
             <button className={[styles.navButton, styles.active].join(" ")} active="true">
                 Login
