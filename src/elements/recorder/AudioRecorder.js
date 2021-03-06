cd593afe4268606ab53b4c22c41c2671985b2ed4
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-03-06 12:01:51 +0000

    Microphone Button UI + Creation of Media Recorder

--- src/elements/recorder/AudioRecorder.js
+++ src/elements/recorder/AudioRecorder.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import styles from "./AudioRecorder.module.css";
 
+const TIME_INTERVAL = 500;
+
 class AudioRecorder extends Component {
     state = {
         recording: false,
     };
 
+    componentDidMount() {
+        this.prepareMicrophone();
+    }
+
+    prepareMicrophone = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.addEventListener("dataavailable", this.onStart);
+        mediaRecorder.addEventListener("stop", this.onStop);
+    };
+
+    onStart = ({ data }) => {
+        console.log(data);
+    };
+
+    onStop = ({ data }) => {
+        console.log(data);
+    };
+
     onClick = () => {
+        if (false) this.mediaRecorder.start(TIME_INTERVAL);
         this.setState({ recording: !this.state.recording });
     };
 
